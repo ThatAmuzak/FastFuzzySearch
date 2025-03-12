55990bd5501656dd5a26e631fb4b9e21e47b7079
ThatAmuzak <rishavbanerjee1710@gmail.com>
2025-03-12 16:42:02 -0700

    implemented API for fuzzy search

--- Runtime/FuzzySearch.cs
+++ Runtime/FuzzySearch.cs
@@ -1,7 +1,74 @@
+using System;
+using System.Collections.Generic;
+
 namespace FastFuzzySearch
 {
     public static class FuzzySearch
     {
+        public static string GetBestOne(string targetWord, List<string> wordList, MatchingTechnique technique = MatchingTechnique.LevenshteinDistance, bool enableBonusForExactMatch = false)
+        {
+            if (string.IsNullOrWhiteSpace(targetWord)) return "";
+            return GetBestN(targetWord, wordList, 1, technique, enableBonusForExactMatch)[0];
+        }
 
+        public static List<string> GetBestN(string targetWord, List<string> wordList, int returnCount, MatchingTechnique technique = MatchingTechnique.LevenshteinDistance, bool enableBonusForExactMatch = false)
+        {
+            if (string.IsNullOrWhiteSpace(targetWord)) return null;
+            targetWord = targetWord.ToLower();
+            int globalBestOfList = Int32.MaxValue;
+            List<KeyValuePair<float, string>> closestMatches = new List<KeyValuePair<float, string>>();
+            foreach (string word in wordList)
+            {
+                int distance = -1;
+                switch (technique)
+                {
+                    case MatchingTechnique.LevenshteinDistance:
+                        distance = StringMatchingLogic.LevenshteinDistance(targetWord, word.ToLower(), globalBestOfList);
+                        break;
+                    case MatchingTechnique.HammingDistance:
+                        distance = StringMatchingLogic.HammingDistance(targetWord, word.ToLower(), globalBestOfList);
+                        break;
+                }
+
+                if (distance < 0) continue;
+                float ratio = distance / (float) Math.Max(targetWord.Length, word.Length);
+                if (enableBonusForExactMatch)
+                {
+                    if (word.ToLower().Contains(targetWord)) ratio -= 0.5f;
+                    if (ratio < 0) ratio = 0;
+                }
+
+                if (closestMatches.Count <= 0)
+                {
+                    closestMatches.Add(new KeyValuePair<float, string>(ratio, word));
+                }
+                else
+                {
+                    for (int index = 0; index < closestMatches.Count; index++)
+                    {
+                        if (index >= returnCount) break;
+                        if (closestMatches[index].Key > ratio)
+                        {
+                            closestMatches.Insert(index, new KeyValuePair<float, string>(ratio, word));
+                            break;
+                        }
+                    }
+                    if(closestMatches.Count > returnCount) closestMatches.RemoveAt(closestMatches.Count - 1);
+                }
+            }
+
+            List<string> closestMatchesWords = new List<string>();
+            foreach (KeyValuePair<float, string> matches in closestMatches)
+            {
+                closestMatchesWords.Add(matches.Value);
+            }
+            return closestMatchesWords;
+        }
+    }
+
+    public enum MatchingTechnique
+    {
+        LevenshteinDistance = 0,
+        HammingDistance = 1
     }
 }
--- Runtime/StringMatchingLogic.cs
+++ Runtime/StringMatchingLogic.cs
@@ -4,16 +4,22 @@ namespace FastFuzzySearch
 {
     public static class StringMatchingLogic
     {
-        public static int HammingDistance(string target, string toCompare, int globalBest = -1)
+        public static int HammingDistance(string target, string toCompare, int globalBest)
         {
             if (target.Length != toCompare.Length) return -1;
             int distance = 0;
             for (int i = 0; i < target.Length; i++)
-                if (target[i] != toCompare[i]) distance++;
+            {
+                if (target[i] != toCompare[i])
+                {
+                    distance++;
+                    if (distance > globalBest) return -1;
+                }
+            }
             return distance;
         }
 
-        public static int LevenshteinDistance(string target, string toCompare, int globalBest = -1)
+        public static int LevenshteinDistance(string target, string toCompare, int globalBest)
         {
             int targetLength = target.Length, toCompareLength = toCompare.Length;
             int[,] distanceMatrix = new int[targetLength + 1, toCompareLength + 1];
@@ -23,6 +29,7 @@ namespace FastFuzzySearch
                 distanceMatrix[0, j] = j;
             for (int j = 1; j < toCompareLength + 1; j++)
             {
+                int rowMin = int.MaxValue;
                 for (int i = 1; i < targetLength + 1; i++)
                 {
                     int cost = target[i - 1] != toCompare[j - 1] ? 1 : 0;
@@ -30,7 +37,10 @@ namespace FastFuzzySearch
                     int ins = distanceMatrix[i - 1, j] + 1;
                     int del = distanceMatrix[i, j - 1] + 1;
                     distanceMatrix[i, j] = Math.Min(sub, Math.Min(ins, del));
+                    if (distanceMatrix[i, j] < rowMin) rowMin = distanceMatrix[i, j];
                 }
+
+                if (rowMin >= globalBest) return -1;
             }
             return distanceMatrix[targetLength, toCompareLength];
         }
